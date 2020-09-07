b948c370aa3f7d8e18c02a9a0ecf609b85627886
Conner <64811824+ConnerSick@users.noreply.github.com>
2020-09-06 20:15:14 -0700

    Update app.js

---
+++ static/app.js
@@ -0,0 +1,16 @@
+function doSomething(sample) {
+    d3.json('../samples.json').then((data)  => {
+        var metadata = data.metadata
+        var resultArray = metadata.filter(sampleObj => sampleObj.id == sample)
+        var result = resultArray[0]
+        var testVar = d3.select('#sample-metadata')
+
+        testVar.html("")
+        Object.entries(result).forEach(([key,value]) => {
+            testVar.append('h6').text('${key.toUpperCase()}: ${value}')
+        })
+    })
+}
+
+
+console.log(testVar[0])
