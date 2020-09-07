10326af2519f37d90a0ab5e203975ebcb2e02a7e
Conner <64811824+ConnerSick@users.noreply.github.com>
2020-09-06 20:28:27 -0700

    Update app.js

--- static/app.js
+++ static/app.js
@@ -1,15 +1,38 @@
-function doSomething(sample) {
+function getData(sample) {
     d3.json('../samples.json').then((data)  => {
         var metadata = data.metadata
         var resultArray = metadata.filter(sampleObj => sampleObj.id == sample)
         var result = resultArray[0]
-        var testVar = d3.select('#sample-metadata')
+        var dataVar = d3.select('#sample-metadata')
 
-        testVar.html("")
+        dataVar.html("")
         Object.entries(result).forEach(([key,value]) => {
-            testVar.append('h6').text('${key.toUpperCase()}: ${value}')
+            dataVar.append('h6').text('${key.toUpperCase()}: ${value}')
         })
     })
 }
 
 
+function createCharts(sample) {
+    d3.json('../samples.json').then((data) => {
+        var samples = data.samples
+        var resultArray = samples.filter(sampleObj => sampleObj.id == sample)
+        var result = resultArray[0]
+        var sample_values = result.sample_values
+        var otu_ids = result.otu_ids
+        var otu_labels = result.otu_labels
+
+        var bubbleLayout = {
+            title: "Bacteria Cultures Per Sample",
+            margin: 
+
+
+
+
+        }
+        
+        
+        
+        )
+    })
+}
